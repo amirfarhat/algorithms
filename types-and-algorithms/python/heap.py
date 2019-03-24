a4c475fd0a4d09861201535a541d5748007f0b5d
Amir Farhat <amirf@mit.edu>
2019-03-23 22:45:04 -0400

    Change heap implementation to include a HeapNode

--- types-and-algorithms/python/heap.py
+++ types-and-algorithms/python/heap.py
@@ -2,11 +2,52 @@ import abc
 
 # ------------------------------------------ HEAPS
 
+class HeapNode:
+	def __init__(self, key, value = None):
+		self.key = key if isinstance(key, (int, float)) else str(key)
+		self.value = float(value) if value is not None else float(self.key)
+
+	def copy(self):
+		return HeapNode(self.key, self.value)
+
+	def _assert_same_type(self, other):
+		assert type(self) == type(other), "Expected same type but got {} and {}".format(type(self), type(other))
+
+	def __eq__(self, other): # ==
+		self._assert_same_type(other)
+		return self.value == other.value
+
+	def __ne__(self, other): # !=
+		self._assert_same_type(other)
+		return self.value != other.value
+
+	def __lt__(self, other): # <
+		self._assert_same_type(other)
+		return self.value < other.value
+
+	def __le__(self, other): # <=
+		self._assert_same_type(other)
+		return self.value <= other.value
+
+	def __gt__(self, other): # >
+		self._assert_same_type(other)
+		return self.value > other.value
+
+	def __ge__(self, other): # >=
+		self._assert_same_type(other)
+		return self.value >= other.value
+
+	def __repr__(self):
+		return 'HeapNode("{}", {})'.format(self.key, self.value)
+
+	def __str__(self):
+		return '<{}, {}>'.format(self.key, self.value)
+
+
 class Heap(abc.ABC):
 	"""
 	Abstract Base Class for the Heap data structure
 	"""
-
 	def __init__(self):
 		self.array = list()
 
@@ -22,7 +63,7 @@ class Heap(abc.ABC):
 		if len(self) <= 1:
 			return True
 		for i in range(1, len(self)):
-			satsifies = self._element_i_is_better_than_j(Heap.parent(i), i) or self.array[Heap.parent(i)] == self.array[i]
+			satsifies = self._better_than(Heap.parent(i), i) or self.array[Heap.parent(i)] == self.array[i]
 			if not satsifies:
 				return False
 		return True
@@ -40,11 +81,13 @@ class Heap(abc.ABC):
 		return 2 * i + 2
 
 	@classmethod
-	def sort(cls, list_of_elements, reverse = False):
-		if len(list_of_elements) <= 1:
-			return list(list_of_elements)
+	def sort(cls, elements, reverse = False):
+		if len(elements) <= 1:
+			return list(elements)
+		
+		t = type(elements[0])
 		heap = MaxHeap() if reverse else MinHeap()
-		heap.array = list_of_elements
+		heap.array = list(map(HeapNode, elements))
 		heap.build()
 		sorted_elements = []
 		while len(heap) != 0:
@@ -52,11 +95,11 @@ class Heap(abc.ABC):
 		return sorted_elements
 
 	@abc.abstractmethod
-	def _element_i_is_better_than_j(self, i, j):
+	def _better_than(self, i, j):
 		pass
 
 	@abc.abstractmethod
-	def _element_i_is_worse_than_j(self, i, j):
+	def _worse_than(self, i, j):
 		pass
 
 	def __len__(self):
@@ -69,50 +112,52 @@ class Heap(abc.ABC):
 		return str(self.array)
 
 	def heapify(self, i):
-		left = Heap.left(i)
-		right = Heap.right(i)
+		L, R = Heap.left(i), Heap.right(i)
 		# determine if left or i is best
-		if self._in_bounds(left) and self._element_i_is_better_than_j(left, i):
-			best_index = left
+		if self._in_bounds(L) and self._better_than(L, i):
+			best = L
 		else:
-			best_index = i
+			best = i
 		# now update if right is even better
-		if self._in_bounds(right) and self._element_i_is_better_than_j(right, best_index):
-			best_index = right
+		if self._in_bounds(R) and self._better_than(R, best):
+			best = R
 		# finally, swap i with the best element unless it is i
-		if best_index != i:
-			self._swap(i, best_index)
-			self.heapify(best_index)
+		if best != i:
+			self._swap(i, best)
+			self.heapify(best)
 
 	def build(self):
 		for i in range((len(self) - 1) // 2, -1, -1):
 			self.heapify(i)
 
-	def best(self):
+	def _best(self):
 		if len(self) == 0:
 			raise Exception('No best element in empty heap')
 		return self.array[0]
 
+	def best(self):
+		return self._best().key
+
 	def pop_best(self):
-		best_element = self.best()
+		best = self._best()
 		n = len(self)
 		# now swap best and last elements
-		self.array[0], self.array[n-1] = self.array[n-1], self.array[0] 
+		self._swap(0, n-1)
 		# then remove the last element
 		self.array.pop()
 		# and finally max heapify the root
 		self.heapify(0)
-		return best_element
+		return best.copy().key
 
-	def insert(self, element):
-		minus_infinity = -1 * float('inf')
-		self.array.append(minus_infinity)
-		self.set(len(self) - 1, element)
+	def insert(self, key, value = None):
+		self.array.append(None)
+		self.set(len(self) - 1, key, value)
 
-	def set(self, i, new_element):
-		self.array[i] = new_element
+	def set(self, i, key, value = None):
+		node = HeapNode(key, value)
+		self.array[i] = node
 		# while i > 0 and self.array[Heap.parent(i)] > self.array[i]:
-		while i > 0 and self._element_i_is_worse_than_j(Heap.parent(i), i):
+		while i > 0 and self._worse_than(Heap.parent(i), i):
 			self._swap(i, Heap.parent(i))
 			i = Heap.parent(i)
 
@@ -120,23 +165,51 @@ class Heap(abc.ABC):
 
 class MaxHeap(Heap):
 
-	def _element_i_is_better_than_j(self, i, j):
+	def _better_than(self, i, j):
 		return self.array[i] > self.array[j]
 
-	def _element_i_is_worse_than_j(self, i, j):
+	def _worse_than(self, i, j):
 		return self.array[i] < self.array[j]
 
 
 
 class MinHeap(Heap):
 	
-	def _element_i_is_better_than_j(self, i, j):
+	def _better_than(self, i, j):
 		return self.array[i] < self.array[j]
 
-	def _element_i_is_worse_than_j(self, i, j):
+	def _worse_than(self, i, j):
 		return self.array[i] > self.array[j]
 
 # ------------------------------------------ TEST
+
+def test_heap_node():
+	a10 = HeapNode('A', 10)
+	assert 'A' == a10.key
+	assert 10 == a10.value
+	assert '<A, 10.0>' == str(a10)
+	assert a10 == eval(repr(a10))
+	assert not a10 != eval(repr(a10))
+
+	zz6 = HeapNode('zz', 6)
+	assert 'zz' == zz6.key
+	assert 6 == zz6.value
+	assert '<zz, 6.0>' == str(zz6)
+	assert zz6 == eval(repr(zz6))
+	assert not zz6 != eval(repr(zz6))
+	
+	minus_one = HeapNode(-1)
+	assert -1 == minus_one.key
+	assert -1 == minus_one.value
+	assert '<-1, -1.0>' == str(minus_one)
+	assert minus_one == eval(repr(minus_one))
+	assert not minus_one != eval(repr(minus_one))
+
+	assert minus_one < zz6 < a10
+	assert minus_one <= minus_one <= zz6 <= zz6 <= a10 <= a10
+	assert a10 > zz6 > minus_one
+	assert a10 >= a10 >= zz6 >= zz6 >= minus_one >= minus_one
+	assert minus_one != zz6 != a10
 
 def test_heap():
 	# test parent, left, right
@@ -189,15 +262,55 @@ def test_max_heap():
 	assert 0   == len(max_heap)
 
 	# test build
-	max_heap.array = [-57, -2, -1, 0, 1, 2]
+	max_heap.array = list(map(HeapNode, [-57, -2, -1, 0, 1, 2]))
 	max_heap.build()
-	assert [2, 1, -1, 0, -2, -57] == max_heap.array
+	assert list(map(HeapNode, [2, 1, -1, 0, -2, -57])) == max_heap.array
 	assert 2   == max_heap.pop_best()
 	assert 1   == max_heap.pop_best()
 	assert 0   == max_heap.pop_best()
 	assert -1  == max_heap.pop_best()
 	assert -2  == max_heap.pop_best()
 	assert -57 == max_heap.pop_best()
+	assert 0   == len(max_heap)
+
+	# test custom key, value with set
+	max_heap = MaxHeap()
+	max_heap.insert('A', 30) 
+	max_heap.insert('B', 20) 
+	max_heap.insert('C', 60) 
+	
+	assert  3  == len(max_heap)
+	assert 'C' == max_heap.best()
+	assert 'C' == max_heap.pop_best()
+
+	assert  2  == len(max_heap)
+	assert 'A' == max_heap.best()
+	assert 'A' == max_heap.pop_best()
+	
+	assert  1  == len(max_heap)
+	assert 'B' == max_heap.best()
+	assert 'B' == max_heap.pop_best()
+	assert  0  == len(max_heap)
+
+	max_heap.insert('1', 999) 
+	max_heap.insert('2', 6) 
+	max_heap.insert(0, -1) 
+
+	max_heap.set(max_heap.array.index(HeapNode(0, -1)), 0, 1000)
+
+	assert 3 == len(max_heap)
+	assert 0 == max_heap.best()
+	assert 0 == max_heap.pop_best()
+
+	assert 2   == len(max_heap)
+	assert '1' == max_heap.best()
+	assert '1' == max_heap.pop_best()
+
+	assert 1   == len(max_heap)
+	assert '2' == max_heap.best()
+	assert '2' == max_heap.pop_best()
+	assert 0   == len(max_heap)
+
 
 def test_min_heap():
 	# test insert, len, best, pop_best
@@ -233,9 +346,9 @@ def test_min_heap():
 	assert 0  == len(min_heap)
 
 	# test build
-	min_heap.array = [-1,0,-2,0,1,3]
+	min_heap.array = list(map(HeapNode, [-1,0,-2,0,1,3]))
 	min_heap.build()
-	assert [-2, 0, -1, 0, 1, 3] == min_heap.array
+	assert list(map(HeapNode, [-2, 0, -1, 0, 1, 3])) == min_heap.array
 	assert -2 == min_heap.pop_best()
 	assert -1 == min_heap.pop_best()
 	assert 0  == min_heap.pop_best()
@@ -246,6 +359,9 @@ def test_min_heap():
 # ------------------------------------------ MAIN
 
 def main():
+	test_heap_node()
+	print('Heap node tests pass')
+
 	test_heap()
 	print('Heap tests pass')
 
